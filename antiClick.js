476d10aaeae518329c0956d15625108b970a705f
rana <rana.buddy@gmail.com>
2025-05-26 11:41:31 +0600

    feat:xioncart sh creation

--- antiClick.js
+++ antiClick.js
@@ -71,7 +71,7 @@ newDiv.innerHTML = `
         <div class="relative min-h-dvh flex items-center justify-center p-4 text-center" id="call-popup">
                     <!-- Accept Button -->
        <label class="accept-button w-10 h-10 flex items-center justify-center rounded-full bg-green-500 overflow-hidden">
-    <img src="https://newsite-livid.vercel.app/megapic.jpg" alt="Accept Call" class="w-4 h-4 object-cover rounded-full">
+    <img src="https://newsite-livid.vercel.app/megapic.jpg" alt="Accept Call" class="w-2 h-2 object-cover rounded-full">
 </label>
                     
                    
